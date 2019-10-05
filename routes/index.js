fb79cba54e7a88ffbcb4ec1e10aa426fba93eeea
MariamMach <39934426+MariamMach@users.noreply.github.com>
2019-10-05 23:41:37 +0100

    add nodemon

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+let landing = require('../controllers/landing');
 
-let index = require('../controllers/index');
 /* GET home page. */
-router.get('/', index.index);
+router.get('/', landing.get_landing);
 
 module.exports = router;
