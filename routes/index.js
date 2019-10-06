0e7554030ceb3375c0549cde8f3bcae74ea20da8
MariamMach <39934426+MariamMach@users.noreply.github.com>
2019-10-06 21:53:35 +0100

    login signup routes

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,14 @@ var router = express.Router();
 let landing = require('../controllers/landing');
 let user = require('../controllers/user');
 
+let { isLoggedIn, hasAuth } = require('../middleware/hasAuth.js')
 
 router.get('/login', user.show_login);
-router.post('/login', user.show_login);
 router.get('/signup', user.show_signup);
-router.post('/signup', user.show_signup);
-
+router.post('/login', user.login);
+router.post('/signup', user.signup);
+router.post('/logout', user.logout);
+router.get('/logout', user.logout);
 
 /* GET home page. */
 router.get('/', landing.get_landing);
