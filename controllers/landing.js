da7b7e3c332eab8b7ea508b90adb83893d6510a2
MariamMach <39934426+MariamMach@users.noreply.github.com>
2019-10-06 05:55:49 +0100

    show lead

--- controllers/landing.js
+++ controllers/landing.js
@@ -7,13 +7,24 @@ exports.get_landing = function (req, res, next) {
 exports.submit_lead = function (req, res, next) {
     return models.Lead.create({
         email: req.body.lead_email
-    }),then(lead => {
+    }).then(lead => {
         res.redirect('/leads');
     });
 };
 
 exports.show_leads = function (req, res, next) {
-    models.Lead.findAll().then(leads => {
+    return models.Lead.findAll().then(leads => {
         res.render('landing', { title: 'Express' , leads: leads});
+    });
+};
+
+
+exports.show_lead = function (req, res, next) {
+    return models.Lead.findOne({
+        where : {
+            id : req.params.lead_id
+        }
+    }).then(lead => {
+        res.render('lead', { lead : lead })
     });
 };
